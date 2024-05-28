911dd1da15eeab2a99940c75cdc3490f26ed8975
TinoDCodes <tinodauti@gmail.com>
2024-05-28 02:53:46 +0200

    feat: completed mycalculatordapp tutorial

--- programs/mycalculatorapp/src/lib.rs
+++ programs/mycalculatorapp/src/lib.rs
@@ -14,6 +14,39 @@ pub mod mycalculatorapp {
 
         Ok(())
     }
+
+    // ADDITION
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 + num2;
+
+        Ok(())
+    }
+
+    // SUBTRACTION
+    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 - num2;
+
+        Ok(())
+    }
+
+    // MULTIPLICATION
+    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 * num2;
+
+        Ok(())
+    }
+
+    // DIVISION
+    pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 / num2;
+        calculator.remainder = num1 % num2;
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -23,6 +56,30 @@ pub struct Create<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Addition<'info> {
+    #[account(mut)]
+   pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Subtraction<'info> {
+    #[account(mut)]
+   pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Multiplication<'info> {
+    #[account(mut)]
+   pub calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Division<'info> {
+    #[account(mut)]
+   pub calculator: Account<'info, Calculator>
 }
 
 #[account]
--- tests/mycalculatorapp.ts
+++ tests/mycalculatorapp.ts
@@ -1,3 +1,5 @@
+import { it } from "mocha";
+
 const assert = require("assert");
 const anchor = require("@coral-xyz/anchor");
 const { SystemProgram } = anchor.web3;
@@ -9,6 +11,7 @@ describe("mycalculatorapp", () => {
   const calculator = anchor.web3.Keypair.generate();
   const program = anchor.workspace.Mycalculatorapp;
 
+  /*---------- TEST CREATE CALCULATOR ----------*/
   it("Creates a calculator", async () => {
     await program.rpc.create("Welcome to Solana Nash", {
       accounts: {
@@ -23,5 +26,71 @@ describe("mycalculatorapp", () => {
       calculator.publicKey
     );
     assert.ok(account.greeting === "Welcome to Solana Nash");
+  });
+
+  /*---------- TEST CALCULATOR ADDITION ----------*/
+  it("Adds two numbers", async () => {
+    await program.rpc.add(new anchor.BN(20), new anchor.BN(13), {
+      accounts: {
+        calculator: calculator.publicKey,
+      },
+    });
+
+    const actualAnswer = new anchor.BN(20 + 13);
+    const account = await program.account.calculator.fetch(
+      calculator.publicKey
+    );
+
+    assert.ok(account.result.eq(actualAnswer));
+  });
+
+  /*---------- TEST CALCULATOR SUBTRACTION ----------*/
+  it("Subtrcts two numbers", async () => {
+    await program.rpc.subtract(new anchor.BN(54), new anchor.BN(4), {
+      accounts: {
+        calculator: calculator.publicKey,
+      },
+    });
+
+    const actualAnswer = new anchor.BN(50);
+    const account = await program.account.calculator.fetch(
+      calculator.publicKey
+    );
+
+    assert.ok(account.result.eq(actualAnswer));
+  });
+
+  /*---------- TEST CALCULATOR MULTIPLICATION ----------*/
+  it("Multiplies two numbers", async () => {
+    await program.rpc.multiply(new anchor.BN(3), new anchor.BN(5), {
+      accounts: {
+        calculator: calculator.publicKey,
+      },
+    });
+
+    const actualAnswer = new anchor.BN(15);
+    const account = await program.account.calculator.fetch(
+      calculator.publicKey
+    );
+
+    assert.ok(account.result.eq(actualAnswer));
+  });
+
+  /*---------- TEST CALCULATOR DIVISION ----------*/
+  it("Divides two numbers", async () => {
+    await program.rpc.divide(new anchor.BN(33), new anchor.BN(7), {
+      accounts: {
+        calculator: calculator.publicKey,
+      },
+    });
+
+    const actualAnswer = new anchor.BN(4);
+    const actualRemainder = new anchor.BN(5);
+    const account = await program.account.calculator.fetch(
+      calculator.publicKey
+    );
+
+    assert.ok(account.result.eq(actualAnswer));
+    assert.ok(account.remainder.eq(actualRemainder));
   });
 });
